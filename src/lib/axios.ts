8505b497ba423885c3c33334ce142f78b291ff81
Phu Tho <phanphutho9999@gmail.com>
2025-09-25 12:12:06 +0700

    ci(dotenvx): inject DOTENV_PRIVATE_KEY for secure env decryption

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -2,7 +2,7 @@ import app from '@/configs/app'
 import axios from 'axios'
 
 export const api = axios.create({
-	baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8080',
+	baseURL: process.env.NEXT_PUBLIC_BASE_URL || 'http://localhost:8080',
 	headers: {
 		'Content-Type': 'application/json',
 	},
