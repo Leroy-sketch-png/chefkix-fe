bdcf0a870dfca56117533e3ae9c2e144f3579822
Phu Tho <phanphutho9999@gmail.com>
2025-12-01 11:49:55 +0700

    feat(completion): add PostDeadlineCard component with urgency states

--- src/components/completion/index.ts
+++ src/components/completion/index.ts
@@ -5,3 +5,17 @@ export { ImmediateRewards } from './ImmediateRewards'
 export { FirstCookCelebration } from './FirstCookCelebration'
 export { LevelUpCelebration, LevelUpToast } from './LevelUpCelebration'
 export { PostSuccessRewards } from './PostSuccessRewards'
+export {
+	PostDeadlineCard,
+	PostDeadlineStack,
+	PostDeadlineBadge,
+	PostDeadlineMobileStrip,
+} from './PostDeadlineCard'
+export type {
+	PostDeadlineCardProps,
+	PostDeadlineStackProps,
+	PostDeadlineBadgeProps,
+	PostDeadlineMobileStripProps,
+	PendingPost,
+	DeadlineState,
+} from './PostDeadlineCard'
